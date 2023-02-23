bfaf1e50677c41fa1e3d141ee0f6a74419b2c9e8
André Silva <andre06x@gmail.com>
2023-02-23 19:47:59 -0300

    campo curso

--- auth-api/src/modules/estudante/model/Estudante.js
+++ auth-api/src/modules/estudante/model/Estudante.js
@@ -16,6 +16,10 @@ const Estudante = sequelize.define(
       type: Sequelize.STRING,
       allowNull: false,
     },
+    curso: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
     email: {
       type: Sequelize.STRING,
       allowNull: false,
