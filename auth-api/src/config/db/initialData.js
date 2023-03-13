9905ffb18c3b2ff084a6e00294087d0cd4bda55e
André Silva <andre06x@gmail.com>
2023-03-13 18:26:01 -0300

    fix id id_usuario fixo

--- auth-api/src/config/db/initialData.js
+++ auth-api/src/config/db/initialData.js
@@ -4,13 +4,13 @@ import Estudante from "../../modules/estudante/model/Estudante.js";
 
 export async function createInitialData() {
   try {
-    await Estudante.sync({ force: true });
+    // await Estudante.sync({ force: true });
 
     const password = await bcrypt.hash("123456", 10);
 
     await Estudante.create({
-      id: randomUUID(),
-      id_usuario: randomUUID(),
+      id: "7708e029-f30d-40fa-8936-2467edbad6c1",
+      id_usuario: "474d78b4-1797-4238-86e7-9afdd90ce2cc",
       email: "201908274417@alunos.estacio.br",
       matricula: "201908274417",
       campus: "NITEROI",
