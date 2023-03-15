bde42e32cd6e041695bbbf0a2a6f186c0b1be5fe
André Silva <andre06x@gmail.com>
2023-03-15 20:58:41 -0300

    adicionando interceptor token jwt

--- auth-api/src/modules/estudante/model/client/Usuarios.js
+++ auth-api/src/modules/estudante/model/client/Usuarios.js
@@ -3,12 +3,17 @@ import axios from "axios";
 import { USUARIOS_API_URL } from "../../../../config/constants/secrets.js";
 
 class DadosUsuarios {
-  async getDataUser(id_usuario) {
+  async getDataUser(id_usuario, token) {
     try {
       let response = false;
       console.log(id_usuario);
+
+      const headers = {
+        Authorization: token,
+      };
+
       await axios
-        .get(`${USUARIOS_API_URL}/${id_usuario}`)
+        .get(`${USUARIOS_API_URL}/${id_usuario}`, { headers })
         .then((res) => {
           console.log(res.data);
           response = res.data;
