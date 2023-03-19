e86f441e1f83e4fb6fc8f4b1337ecdd47c07f185
André Silva <andre06x@gmail.com>
2023-03-18 21:59:59 -0300

    add query admin repository

--- usuarios-api/src/main/java/com/example/usuariosapi/modules/usuarios/repository/UsuarioRepository.java
+++ usuarios-api/src/main/java/com/example/usuariosapi/modules/usuarios/repository/UsuarioRepository.java
@@ -1,11 +1,18 @@
 package com.example.usuariosapi.modules.usuarios.repository;
 
+import com.example.usuariosapi.modules.usuarios.dto.UsuarioAdminResponse;
 import com.example.usuariosapi.modules.usuarios.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    @Query(value = "SELECT admin FROM usuarios WHERE id = ?1", nativeQuery = true)
+    List<UsuarioAdminResponse> admin(UUID id);
 }
