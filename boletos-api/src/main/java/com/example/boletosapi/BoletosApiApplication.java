34831fe81e32412f21877eccab76f6d61591a9f3
André Silva <andre06x@gmail.com>
2023-03-20 22:44:10 -0300

    base boletos-api

---
+++ boletos-api/src/main/java/com/example/boletosapi/BoletosApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.boletosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletosApiApplication.class, args);
+	}
+
+}
---
+++ boletos-api/src/test/java/com/example/boletosapi/BoletosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boletosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
