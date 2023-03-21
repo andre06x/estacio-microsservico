f8349a06affe682c0683a185404faa261eb9d348
André Silva <andre06x@gmail.com>
2023-03-20 22:44:26 -0300

    base disciplinas-api

---
+++ disciplinas-api/src/main/java/com/example/disciplinasapi/DisciplinasApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.disciplinasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplinasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplinasApiApplication.class, args);
+	}
+
+}
