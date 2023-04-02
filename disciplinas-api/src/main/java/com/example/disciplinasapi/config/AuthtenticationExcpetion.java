525e81cf6b16890b1584d31ecb267582251d79c9
André Silva <andre06x@gmail.com>
2023-04-02 16:32:14 -0300

    config auth jwt

---
+++ disciplinas-api/src/main/java/com/example/disciplinasapi/config/AuthtenticationExcpetion.java
@@ -0,0 +1,12 @@
+package com.example.disciplinasapi.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class AuthtenticationExcpetion extends RuntimeException{
+
+    public AuthtenticationExcpetion(String message){
+        super(message);
+    }
+}
