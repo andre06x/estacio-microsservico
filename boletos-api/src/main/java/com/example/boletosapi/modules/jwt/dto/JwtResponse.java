6489a503aa6ca00eef7168f2d4292a3806504ba3
André Silva <andre06x@gmail.com>
2023-04-07 14:00:33 -0300

    add jwt

---
+++ boletos-api/src/main/java/com/example/boletosapi/modules/jwt/dto/JwtResponse.java
@@ -0,0 +1,32 @@
+package com.example.boletosapi.modules.jwt.dto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+
+    private UUID id;
+    private UUID id_usuario;
+    private String email;
+    private Boolean admin;
+
+    public static JwtResponse getUser(Claims jwtClaims){
+        try {
+            return new ObjectMapper().convertValue(jwtClaims.get("authUser"), JwtResponse.class);
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+
+    }
+}
